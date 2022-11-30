399d77827032ab6f1185c0d80e5055f377676c1f
unknown <luismatthews29@gmail.com>
2022-11-30 17:10:42 -0300

    Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,91 @@
-import { useState } from 'react';
+import { useState,ChangeEvent } from 'react';
 
 import { Header }  from './Components/Header';
-import { Input }  from './Components/Input';
-import {List} from './Components/List';
+import { Item }  from './Components/Item';
+
 
 
 import styles from "./App.module.css";
 import './global.css';
 
+export interface todoProps {
+
+  content:string;
+  id:string;
+  finished:boolean;
+}
+
+
+
 function App() {
+  
+  const [todoItems,setTodoItems] = useState<todoProps[]>([]);
+  
+  const [newTodoText, setNewTodoText] = useState('');
+  
+
+
+  function handleNewText(event:ChangeEvent<HTMLTextAreaElement>){
+
+    setNewTodoText(event.target.value);
+  }
+  function handleGetNewTodoText() {
+    // setar nova tarefa
+    console.log(newTodoText);
+  }
+
+
 
   return (
+
     <div className={styles.App}>
       
       <Header/>
-      <Input/>
-      <List/>
+
+
+
+      <div className={styles.container}> 
+            <textarea placeholder='Adicione uma nova tarefa...'
+               onChange={handleNewText}
+               className={styles.textArea}
+            />
+            <button 
+               className={styles.newBtn}
+               onClick={handleGetNewTodoText}
+               >
+               Criar
+               +
+            </button>
+      </div>  
+
+      <section className={styles.listContainer}>
+            <header className={styles.types}>
+               <a>    
+                    Tarefas Criadas
+                  <span className={styles.counter}>0</span>
+               </a> 
+               <a> 
+                        Concluídas 
+                  <span className={styles.counter}>0</span>
+               </a>
+            </header>
+
+            <div className={styles.itemsContainer}> 
+               {/* <img className={styles.logoEmpty} src={logoEmpty}/>
+                  <p>
+                     <span className={styles.bold}>
+                        Você ainda não tem tarefas cadastradas
+                     </span> 
+                     <br/>
+                     Crie tarefas e organize seus itens a fazer   
+                  </p> */}
+              <Item/>
+
+
+            </div>
+      </section>
     </div>
+
   )
 }
 
--- src/Components/Item.tsx
+++ src/Components/Item.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Trash} from 'phosphor-react'
 
 import styles from './Item.module.css'
 
 export function Item(){
   return (
       <div className={styles.item}>
-         <h1>Hello world</h1>
+        <div className={styles.check}></div>
+        <p className={styles.text}>
+        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+        </p>
+        <button>
+          <Trash size={24}/>
+        </button>
       </div>
    
   );
